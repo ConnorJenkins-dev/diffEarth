56fe89fc49c14244f9757d2b9f5a5c065f88178d
Connor <jenkinsc30@cardiff.ac.uk>
2025-02-05 16:34:50 +0000

    setup vite and vue, started developing dashboard from wireframes

---
+++ untitled/src/components/UserDashboard.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+<style>
+
+</style>
+
+<template>
+ <body class="flex flex-row w-full">
+  <section class="w-3/5 m-1 border-2">
+    <div id="nav" class="">
+      <nav class="flex w-full">
+        <ul class="flex flex-wrap justify-between w-full">
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button">Label</button>
+          </li>
+          <li class="flex-1 text-center">
+            <button class="filter-button pi-filter">Filter</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+  <aside class="w-2/5 m-1 border-2">
+    <map>
+      <img src="https://cdn-icons-png.flaticon.com/512/1865/1865269.png" class="w-fit mx-3">
+    </map>
+  </aside>
+ </body>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ untitled/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import Dashboard from "@/components/UserDashboard.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
