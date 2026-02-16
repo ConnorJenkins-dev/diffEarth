d9424060196e3e52fa6734333c9b202abb1de98c
Connor <jenkinsc30@cardiff.ac.uk>
2026-02-16 03:37:25 +0000

    Added dockerignore and dockerfile. Currently builds. Will attempt depoyment

--- diffEarth/vite.config.ts
+++ diffEarth/vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: "/diffearth/",
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/ts/app.ts"],
